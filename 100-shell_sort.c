244b7c6e0d5e13b2e4b547d2c2c13f51b201b923
samuelkchris <samuelkchris@gmail.com>
2023-01-24 10:55:51 +0300

    work load fix

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,40 +1,34 @@
 #include "sort.h"
+
 /**
- * shell_sort - sorts an array using the Shell sort algorithm
- * @array: array to be sorted
- * @size: size of array
- * Return: is void
+ * shell_sort - sorts an array of integers in ascending order using the
+ * Shell sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
+ *
+ * Return: void
  */
 void shell_sort(int *array, size_t size)
 {
-	int temp;
-	size_t gap, i, j;
+	size_t i, j, gap = 1;
+	int insert;
 
-	if (size == 0 || size == 1)
+	if (array == NULL || size < 2)
 		return;
 
-	gap = 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-	while (gap < size)
-	{
-		gap = gap * 3 - 1;
-	}
-
-	gap = (gap - 1) / 3;
 	while (gap > 0)
 	{
-		i = gap;
-		while (i < size)
+		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
-			for (j = i ; j >= gap && array[j - gap] > temp ; j -= gap)
-			{
+			insert = array[i];
+			for (j = i; j >= gap && array[j - gap] > insert; j = j - gap)
 				array[j] = array[j - gap];
-			}
-			array[j] = temp;
-			i++;
+			array[j] = insert;
 		}
-		print_array(array, size);
 		gap = (gap - 1) / 3;
+		print_array(array, size);
 	}
 }
